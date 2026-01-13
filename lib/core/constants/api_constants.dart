4673e1b4b1d5b5a8226ba3fd3141d2dd58fdd7fd
Turgay Yücel <turgay_yucel@aof.anadolu.edu.tr>
2026-01-13 21:31:46 +0300

    Update app name to MoneyPlan Pro in all documentation and update API URL

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,8 +1,9 @@
 class ApiConstants {
   // Localhost for Simulator/Emulator
   // Android Emulator uses 10.0.2.2 usually, iOS Simulator uses localhost
-  static const String baseUrl = 'http://104.247.166.225:8000';
-  
+  static const String baseUrl =
+      'https://invest-guide-api-wandering-snowflake-2603.fly.dev';
+
   // Real Device (IP address of your computer)
-  // static const String baseUrl = 'http://192.168.1.X:8000'; 
+  // static const String baseUrl = 'http://192.168.1.X:8000';
 }
